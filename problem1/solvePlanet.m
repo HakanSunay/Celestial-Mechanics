29c77c52a3f321703dd9af31be102de5e922b408
HakanSunay <hakansunay@abv.bg>
2019-06-21 22:43:33 +0300

    Fix X-axis rotation matrix

--- problem1/solvePlanet.m
+++ problem1/solvePlanet.m
@@ -7,9 +7,9 @@
             sin(tita),  cos(tita), 0;
                     0,          0, 1];
                    
-       I = [cos(i), 0, -sin(i);
-                 0, 1,    0   ;
-            sin(i), 0,  cos(i)];
+       I = [1,      0,       0;
+            0, cos(i), -sin(i);
+            0, sin(i), cos(i)];
              
        G = [cos(g), -sin(g), 0;
             sin(g),  cos(g), 0;
@@ -20,7 +20,7 @@
     gama = 1 + miu;
     n = sqrt(gama / a^3);
     to = ((w - L) / n) * pi/180;
-    l = n * (-t * 2*pi - to);
+    l = n * (t * 2*pi - to);
     u = l + e * sin(l + e * sin(l + e * sin(l)));
     r = Q * a * [cos(u) - e; sin(u) * sqrt(1 - e^2); 0]
     v = Q * [-sin(u); cos(u) * sqrt(1 - e^2);0] * a * n / (1 - e*cos(u))
